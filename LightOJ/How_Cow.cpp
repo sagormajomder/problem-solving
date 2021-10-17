ecc46de23d3e0e6dfec01f9180ac7ba9f3bcc11b
sagormajomder <sagormajomder112@gmail.com>
2021-10-17 20:32:42 +0600

    This week Solved 3 new Lighoj Problem

---
+++ LightOJ/How_Cow.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    for (int  i = 1; i <= t; i++)
+    {
+        cout<<"Case "<<i<<":"<<endl;
+        int x1,y1,x2,y2;
+        cin>>x1>>y1>>x2>>y2;
+        int M;
+        cin>>M;
+        for (int i = 0; i < M; i++)
+        {
+            int x,y;
+            cin>>x>>y;
+            (x>x1 && x<x2 && y>y1 && y<y2)? cout<<"Yes"<<endl : cout<<"No"<<endl;
+        }
+        
+        
+    }
+    
+}
---
+++ LightOJ/Redirect_URL.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    for(int i=1;i<=t;i++){
+        string s;
+        cin>>s;
+        (s[4]!='s')?cout<<"Case "<<i<<": "<<s.insert(4,"s")<<endl: cout<<"Case "<<i<<": "<<s<<endl;
+    }
+}
