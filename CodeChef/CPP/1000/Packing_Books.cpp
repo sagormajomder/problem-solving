d1c606ce2682e37da4b1bbe17b4262f445911f6b
sagormajomder <sagormajomder@gmail.com>
2022-11-04 10:48:07 +0600

    two codeshef probelm solved

---
+++ CodeChef/CPP/1000/Packing_Books.cpp
@@ -0,0 +1,43 @@
+// https://www.codechef.com/problems/BOOKPACK
+/*
+Consistency is the key of success
+Always do a single thing at a particular time, not many
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define F first
+#define S second
+#define pb push_back
+#define bug(...) __f(#__VA_ARGS__, __VA_ARGS__)
+template <typename Arg1>
+void __f(const char *name, Arg1 &&arg1)
+{
+    cout << name << " : " << arg1 << endl;
+}
+template <typename Arg1, typename... Args>
+void __f(const char *names, Arg1 &&arg1, Args &&...args)
+{
+    const char *comma = strchr(names + 1, ',');
+    cout.write(names, comma - names) << " : " << arg1 << " | ";
+    __f(comma + 1, args...);
+}
+void solve()
+{
+    int x, y, z, ans = 0;
+    cin >> x >> y >> z;
+    ans = ans + (y / z);
+    if (y % z)
+        ans++;
+    ans = ans * x;
+    cout << ans << endl;
+}
+int main()
+{
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
