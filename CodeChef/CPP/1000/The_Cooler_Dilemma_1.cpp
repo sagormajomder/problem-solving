34062082efcbefefbdd398501047f4db574e6c1e
Sagor Majomder <sagormajomder@gmail.com>
2023-04-24 11:38:22 +0600

    solve two problem in codechef

---
+++ CodeChef/CPP/1000/The_Cooler_Dilemma_1.cpp
@@ -0,0 +1,43 @@
+/*
+Consistency is the key of success
+Always do a single thing at a particular time, not many
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define F first
+#define S second
+#define pb push_back
+#define bug(...) __f(#__VA_ARGS__, __VA_ARGS__)
+template <typename Arg1>
+void __f(const char *name, Arg1 &&arg1)
+{
+  cout << name << " : " << arg1 << endl;
+}
+template <typename Arg1, typename... Args>
+void __f(const char *names, Arg1 &&arg1, Args &&...args)
+{
+  const char *comma = strchr(names + 1, ',');
+  cout.write(names, comma - names) << " : " << arg1 << " | ";
+  __f(comma + 1, args...);
+}
+void solve()
+{
+  int x, y, m;
+  cin >> x >> y >> m;
+  if (x * m < y)
+    cout << "YES";
+  else
+    cout << "NO";
+  cout << endl;
+}
+int main()
+{
+
+  int t = 1;
+  cin >> t;
+  while (t--)
+  {
+    solve();
+  }
+}
