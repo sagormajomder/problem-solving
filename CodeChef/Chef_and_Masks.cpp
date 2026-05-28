4901f738d6eb7f81c8a1ba306d585b7e749a2fc4
Sagor Majomder <sagormajomder@gmail.com>
2026-05-28 13:32:01 +0600

    solved 7 problems

---
+++ CodeChef/Chef_and_Masks.cpp
@@ -0,0 +1,28 @@
+/*
+Consistency is the key of success
+Always do a single thing at a particular time, not many
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define F first
+#define S second
+#define pb push_back
+void solve()
+{
+  int x, y;
+  cin >> x >> y;
+  if (y * 10 <= x * 100)
+    cout << "Cloth" << endl;
+  else
+    cout << "Disposable" << endl;
+}
+int main()
+{
+  int t = 1;
+  cin >> t;
+  while (t--)
+  {
+    solve();
+  }
+}
