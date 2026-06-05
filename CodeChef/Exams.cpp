be841dd7866fc71a3ab21bb0f02ef8182399a6ca
Sagor Majomder <sagormajomder@gmail.com>
2026-06-05 23:43:00 +0600

    solved codechef problems

---
+++ CodeChef/Exams.cpp
@@ -0,0 +1,29 @@
+/*
+Consistency is the key of success
+Always do a single thing at a particular time, not many
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define F first
+#define S second
+#define pb push_back
+void solve()
+{
+  int x, y, z;
+  cin >> x >> y >> z;
+
+  if (x * y / 2 < z)
+    cout << "YES" << endl;
+  else
+    cout << "NO" << endl;
+}
+int main()
+{
+  int t = 1;
+  cin >> t;
+  while (t--)
+  {
+    solve();
+  }
+}
