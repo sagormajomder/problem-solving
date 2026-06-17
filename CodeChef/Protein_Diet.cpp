e9094df4d6001b1363d794ee70b5d8f5ce2ed170
Sagor Majomder <sagormajomder@gmail.com>
2026-06-17 11:30:22 +0600

    solved codechef problems

---
+++ CodeChef/Protein_Diet.cpp
@@ -0,0 +1,28 @@
+/*
+Consistency is the key of success
+Always do a single thing at a particular time, not many
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define F first
+#define S second
+#define pb push_back
+void solve()
+{
+  int x, y;
+  cin >> x >> y;
+  if (x >= y)
+    cout << "YES" << endl;
+  else
+    cout << "NO" << endl;
+}
+int main()
+{
+  int t = 1;
+  // cin >> t;
+  while (t--)
+  {
+    solve();
+  }
+}
