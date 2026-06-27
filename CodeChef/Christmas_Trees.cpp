cb617c98be050da8961ce463d1e44b7a702cded4
Sagor Majomder <sagormajomder@gmail.com>
2026-06-28 00:59:28 +0600

    add solutions for CodeChef problems: Approximate Answer, Christmas Trees, Coldplay Tickets, Giant Wheel, and Number of Credits

---
+++ CodeChef/Christmas_Trees.cpp
@@ -0,0 +1,25 @@
+/*
+Consistency is the key of success
+Always do a single thing at a particular time, not many
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define F first
+#define S second
+#define pb push_back
+void solve()
+{
+  int n, a, m, b;
+  cin >> n >> a >> m >> b;
+  cout << m * b - n * a << endl;
+}
+int main()
+{
+  int t = 1;
+  // cin >> t;
+  while (t--)
+  {
+    solve();
+  }
+}
---
+++ CodeChef/Giant_Wheel.cpp
@@ -0,0 +1,26 @@
+/*
+Consistency is the key of success
+Always do a single thing at a particular time, not many
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define F first
+#define S second
+#define pb push_back
+void solve()
+{
+  int x;
+  cin >> x;
+  x >= 60 ? cout << "Yes" : cout << "No";
+  cout << endl;
+}
+int main()
+{
+  int t = 1;
+  // cin >> t;
+  while (t--)
+  {
+    solve();
+  }
+}
