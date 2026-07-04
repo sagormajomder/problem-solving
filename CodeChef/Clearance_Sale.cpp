90a9dc2184b6a40c275944e5c2353062b920e2c6
Sagor Majomder <sagormajomder@gmail.com>
2026-07-04 21:28:30 +0600

    add solution for CodeChef problem: Clearance Sale

---
+++ CodeChef/Clearance_Sale.cpp
@@ -0,0 +1,24 @@
+/*
+Consistency is the key of success
+Always do a single thing at a particular time, not many
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define F first
+#define S second
+#define pb push_back
+void solve()
+{
+  int x;
+  cin >> x;
+  cout << x / 2 + x << endl;
+}
+int main()
+{
+  int t = 1;
+  while (t--)
+  {
+    solve();
+  }
+}
