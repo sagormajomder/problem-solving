c6d893d492d78eaed02ff25832924a02a207338f
Sagor Majomder <sagormajomder@gmail.com>
2026-07-08 23:03:25 +0600

    add solution for CodeChef problem: Water Park

---
+++ CodeChef/Water_Park.cpp
@@ -0,0 +1,27 @@
+/*
+Consistency is the key of success
+Always do a single thing at a particular time, not many
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define F first
+#define S second
+#define pb push_back
+void solve()
+{
+  int w, h;
+  cin >> w >> h;
+
+  w >= 60 && h <= 130 ? cout << "YES" : cout << "NO";
+  cout << endl;
+}
+int main()
+{
+  int t = 1;
+  // cin >> t;
+  while (t--)
+  {
+    solve();
+  }
+}
